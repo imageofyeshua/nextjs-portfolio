846a5ad5b1a94d69f88e6e6fb47fcf58fe9e92b2
Daniel Park <intheimageofyeshua@gmail.com>
2024-10-10 13:00:37 +0900

    Added 3D Model

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import bg from "../../public/background/autumn-leaves.png";
+import RenderModel from "@/components/RenderModel";
+import Radio from "@/components/models/Radio";
 
 export default function Home() {
   return (
@@ -7,8 +9,16 @@ export default function Home() {
       <Image
         src={bg}
         alt="background-image"
+        fill
         className="w-full h-full object-cover object-center opacity-25"
       />
+
+      <div className="w-full h-screen">
+        {/* navigation and 3d model */}
+        <RenderModel>
+          <Radio />
+        </RenderModel>
+      </div>
     </main>
   );
 }
--- src/components/RenderModel.jsx
+++ src/components/RenderModel.jsx
@@ -1,11 +1,14 @@
-import { View, Text } from "react-native";
+"use client";
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
+import clsx from "clsx";
+import { Environment } from "@react-three/drei";
 
 const RenderModel = ({ children, className }) => {
   return (
-    <Canvas className={clsx("w-screen h-screen relative", className)}>
+    <Canvas className={clsx("w-screen h-screen -z-10 relative", className)}>
       <Suspense fallback={null}>{children}</Suspense>
+      <Environment preset="dawn" />
     </Canvas>
   );
 };
