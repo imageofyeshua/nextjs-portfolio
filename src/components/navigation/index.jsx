75135f93bf06544d42208bfc4d45b10326bbbf9a
Daniel Park <intheimageofyeshua@gmail.com>
2024-10-10 14:43:12 +0900

    Added Icon with Effects

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,26 +1,19 @@
 "use client";
 import { BtnList } from "@/app/data";
 import React from "react";
+import NavButton from "./NavButton";
 
 const Navigation = () => {
   const angleIncrement = 360 / BtnList.length;
   return (
     <div className="w-full fixed h-screen flex items-center justify-center">
-      <div className="w-max flex items-center justify-between relative">
+      <div className="w-max flex items-center justify-between relative animate-spin-slow">
         {BtnList.map((btn, index) => {
           const angleRad = (index * angleIncrement * Math.PI) / 180;
           const radius = "calc(20vw - 1rem)";
           const x = `calc(${radius}*${Math.cos(angleRad)})`;
           const y = `calc(${radius}*${Math.sin(angleRad)})`;
-          return (
-            <button
-              key={index}
-              className="absolute"
-              style={{ transform: `translate(${x}, ${y})` }}
-            >
-              {btn.label}
-            </button>
-          );
+          return <NavButton key={btn.label} x={x} y={y} {...btn} />;
         })}
       </div>
     </div>
