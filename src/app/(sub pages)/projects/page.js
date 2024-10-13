1ebf48f9d744e05dcd54b8b4b33d6b5c88d118b8
Daniel Park <intheimageofyeshua@gmail.com>
2024-10-13 22:43:42 +0900

    Finished Projects Page

--- src/app/(sub pages)/projects/page.js
+++ src/app/(sub pages)/projects/page.js
@@ -1,25 +1,24 @@
 import Image from "next/image";
-import bg from "../../../public/background/autumn-field.png";
+import bg from "../../../../public/background/autumn-field.png";
 import ProjectList from "@/components/projects";
-import { projectsData } from "../data";
+import { projectsData } from "../../data";
 import RenderModel from "@/components/RenderModel";
 import Grapes from "@/components/models/Grapes";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between relative">
+    <>
       <Image
         src={bg}
         alt="background-image"
-        fill
-        className="w-full h-full object-cover object-center opacity-25"
+        className="-z-50 fixed w-full h-full top-0 left-0 object-cover object-center opacity-25"
       />
       <ProjectList projects={projectsData} />
-      <div className="flex items-center justify-center fixed top-20 left-0 h-screen">
+      <div className="flex items-center justify-center fixed top-20 -left-24 h-screen">
         <RenderModel>
           <Grapes />
         </RenderModel>
       </div>
-    </main>
+    </>
   );
 }
